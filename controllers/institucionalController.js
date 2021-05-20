a1cf2367805d1905e06a389568ef2e54630dfa78
Hendy Almeida <hendy.almeida@hotmail.com>
2021-05-19 22:03:54 -0300

    views estaicas com rota

--- controllers/institucionalController.js
+++ controllers/institucionalController.js
@@ -1,6 +1,15 @@
 const institucionalController = {
     index: (request, response) => {
-        return response.render('index');
+        return response.render('index', { titulo: 'Home' });
+    },
+    sobre: (request, response) => {
+        return response.render('sobre', { titulo: 'Sobre' });
+    },
+    servicos: (request, response) => {
+        return response.render('servicos', { titulo: 'Serviços' });
+    },
+    contato: (request, response) => {
+        return response.render('contato', { titulo: 'Contato' });
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,16 @@ const institucionalController = require('../controllers/institucionalController'
 // http://localhost:3000/
 router.get('/', institucionalController.index);
 
+// http://localhost:3000/sobre
+router.get('/sobre', institucionalController.sobre);
+
+// http://localhost:3000/servicos
+router.get('/servicos', institucionalController.servicos);
+
+// http://localhost:3000/contato
+router.get('/contato', institucionalController.contato);
+
+
 // rota para /pets que retorna o método index da petsController
 router.get('/pets', petsController.index);
 
